4277297f48cfbfb6c3eb871a913eb6dc6eb9abb3
Robson J <robsonjrservices@gmail.com>
2023-11-06 10:51:38 -0300

    FEAT: JQuery, exercise 1 done

---
+++ jquery/exercise1.js
@@ -0,0 +1,9 @@
+/*
+1. Ocultar e Mostrar: Crie um botão que, quando clicado, oculte ou 
+mostre um elemento na página.
+*/
+
+$('input[type="button"]').on('click', function(){
+    $('#hide').hide();
+});
+
